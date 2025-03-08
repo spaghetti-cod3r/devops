3d6c7939496a20c1d870350993f671a6517f2c63
Ammar Meslmani <a.meslmani@innopolis.university>
2025-03-08 04:28:15 +0300

    solved task1

--- app_python/app.py
+++ app_python/app.py
@@ -1,13 +1,32 @@
 from flask import Flask, render_template
 import requests
 import re
+import os
 
 app = Flask(__name__)
 
+VISITS = "visits.txt"
+
+if not os.path.exists(VISITS):
+    with open(VISITS, "w") as f:
+        f.write("0")
+
+def increment_visits():
+    # increase the visit counter and save it to the file
+    with open(VISITS, "r+") as f:
+        visits = int(f.read().strip() or 0)
+        visits += 1
+        f.seek(0)
+        f.write(str(visits))
+        f.truncate()
+    return visits
 
 @app.route('/')
 def home():
     try:
+        # increase visits
+        increment_visits()
+        
         # getting the current time in moscow using the WorldTimeAPI
         moscowTimeZoneParam = 'Europe/Moscow'
         time_api_url = (
@@ -35,6 +54,18 @@ def home():
 
         # return a generic error message to the user
         return 'an error occurred, please try again later', 500
+
+
+@app.route('/visits')
+def visits():
+    """Endpoint to display the number of visits."""
+    try:
+        with open(VISITS, "r") as f:
+            visits = f.read().strip()
+        return f"total visits: {visits}"
+    except Exception as e:
+        app.logger.error(f"error while reading visits: {e}")
+        return "unable to retrieve visits count", 500
 
 
 if __name__ == '__main__':
